9e7a524d99ed1b96aa26ed0c17450b4afeffa7e7
Brian <44274838+lolololgy@users.noreply.github.com>
2025-02-28 14:03:34 +0100

    test image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,2 @@
 <h1>{{ $user->name }}</h1>
+<img src="{{ url('/image/fiets.webp') }}"  alt="Private Image">
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\UserController;
 use App\Http\Controllers\HomeController;
 
+Route::get('/', function () {
+    return redirect('/home');
+});
+
 Route::get('/register', [UserController::class, 'showRegistrationForm']);
 Route::post('/register', [UserController::class, 'register'])->name('register');
 
@@ -11,3 +15,14 @@ Route::get('/login', [UserController::class, 'showLoginForm']);
 Route::post('/login', [UserController::class, 'login'])->name('login');
 
 Route::get('/home', [HomeController::class, 'view'])->name('home');
+
+Route::get('/image/{filename}', function ($filename) {
+    $path = storage_path('app/private/' . $filename);
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    return response()->file($path);
+})->middleware('auth');
+
