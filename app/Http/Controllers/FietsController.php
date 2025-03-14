7037465182a5569a9e2742956679205c6eee58c0
Brian <44274838+lolololgy@users.noreply.github.com>
2025-03-14 14:29:43 +0100

    Fiets maken werkt

--- app/Http/Controllers/FietsController.php
+++ app/Http/Controllers/FietsController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Fiets;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
 class FietsController extends Controller
 {
+//        return response()->json(['message' => 'SUCCESS'], 200);
+//        return response()->json(['message' => 'ERROR'], 404);
     public function createBike()
     {
         $user = Auth::user();
@@ -18,8 +22,26 @@ class FietsController extends Controller
         }
     }
 
-    public function storeBike()
+    public function storeBike(Request $request)
     {
-        return "a";
+        $fiets = new Fiets();
+
+        $fiets->naam = $request->input('Naam');
+        $fiets->prijs = $request->input('Prijs');
+        $fiets->voorraad = $request->input('Voorraad');
+        $fiets->productcategorieën = $request->input('Productcategorieën');
+        $fiets->merk = $request->input('Merk');
+        $fiets->sn = $request->input('SN');
+        $fiets->FrameMateriaal = $request->input('FrameMateriaal');
+        $fiets->BatterijTypen = $request->input('BatterijTypen');
+        $fiets->WielMaat = $request->input('WielMaat');
+        $fiets->versnelling = $request->input('Versnelling');
+        $fiets->KleurVarianten = $request->input('KleurVarianten');
+        $fiets->GarantieInMaand = $request->input('GarantieInMaand');
+
+        $fiets->save();
+
+        return response()->json(['message' => 'Fiets is succesvol aangemaakt'], 200);
     }
 }
+
--- resources/views/create-bike.blade.php
+++ resources/views/create-bike.blade.php
@@ -1,73 +1,77 @@
 @vite(['resources/js/app.js'])
-<label for="Naam">Naam:</label>
-<input type="text" id="Naam" name="Naam"><br>
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<form id="bikeForm">
+    <label for="Naam">Naam:</label>
+    <input type="text" id="Naam" name="Naam" required><br>
 
-<label for="Prijs">Prijs:</label>
-<input type="text" id="Prijs" name="Prijs"><br>
+    <label for="Prijs">Prijs:</label>
+    <input type="text" id="Prijs" name="Prijs" required><br>
 
+    <label for="Voorraad">Voorraad:</label>
+    <input type="text" id="Voorraad" name="Voorraad" required><br>
 
+    <label for="Productcategorieën">Productcategorieën:</label>
+    <input type="text" id="Productcategorieën" name="Productcategorieën" required><br>
 
-{{--<label for="Voorraad">Voorraad:</label>--}}
-{{--<input type="text" id="Voorraad" name="Voorraad"><br>--}}
+    <label for="Merk">Merk:</label>
+    <input type="text" id="Merk" name="Merk" required><br>
 
-{{--<label for="Productcategorieën">Productcategorieën:</label>--}}
-{{--<input type="text" id="Productcategorieën" name="Productcategorieën"><br>--}}
+    <label for="SN">Serienummer (SN):</label>
+    <input type="text" id="SN" name="SN" required><br>
 
-{{--<label for="Merk">Merk:</label>--}}
-{{--<input type="text" id="Merk" name="Merk"><br>--}}
+    <label for="FrameMateriaal">Frame Materiaal:</label>
+    <input type="text" id="FrameMateriaal" name="FrameMateriaal" required><br>
 
-{{--<label for="SN">Serienummer (SN):</label>--}}
-{{--<input type="text" id="SN" name="SN"><br>--}}
+    <label for="BatterijTypen">Batterij Typen:</label>
+    <input type="text" id="BatterijTypen" name="BatterijTypen" required><br>
 
-{{--<label for="FrameMateriaal">Frame Materiaal:</label>--}}
-{{--<input type="text" id="FrameMateriaal" name="FrameMateriaal"><br>--}}
+    <label for="WielMaat">Wiel Maat:</label>
+    <input type="text" id="WielMaat" name="WielMaat" required><br>
 
-{{--<label for="BatterijTypen">Batterij Typen:</label>--}}
-{{--<input type="text" id="BatterijTypen" name="BatterijTypen"><br>--}}
+    <label for="Versnelling">Versnelling:</label>
+    <input type="text" id="Versnelling" name="Versnelling" required><br>
 
-{{--<label for="WielMaat">Wiel Maat:</label>--}}
-{{--<input type="text" id="WielMaat" name="WielMaat"><br>--}}
+    <label for="KleurVarianten">Kleur Varianten:</label>
+    <input type="text" id="KleurVarianten" name="KleurVarianten" required><br>
 
-{{--<label for="Versnelling">Versnelling:</label>--}}
-{{--<input type="text" id="Versnelling" name="Versnelling"><br>--}}
+    <label for="GarantieInMaand">Garantie In Maanden:</label>
+    <input type="text" id="GarantieInMaand" name="GarantieInMaand" required><br>
 
-{{--<label for="KleurVarianten">Kleur Varianten:</label>--}}
-{{--<input type="text" id="KleurVarianten" name="KleurVarianten"><br>--}}
-
-{{--<label for="GarantieInMaand">Garantie In Maanden:</label>--}}
-{{--<input type="text" id="GarantieInMaand" name="GarantieInMaand"><br>--}}
-
-<button type="submit">Fiets Aanmaken</button>
-
+    <button type="submit">Fiets Aanmaken</button>
+</form>
 <script>
-    document.querySelector('button').addEventListener('click', function() {
-        var data = {
-            Naam: $('#Naam').value,
-            Prijs: $('#Prijs').value,
-            // Voorraad: document.querySelector('#Voorraad').value,
-            // Productcategorieën: document.querySelector('#Productcategorieën').value,
-            // Merk: document.querySelector('#Merk').value,
-            // SN: document.querySelector('#SN').value,
-            // FrameMateriaal: document.querySelector('#FrameMateriaal').value,
-            // BatterijTypen: document.querySelector('#BatterijTypen').value,
-            // WielMaat: document.querySelector('#WielMaat').value,
-            // Versnelling: document.querySelector('#Versnelling').value,
-            // KleurVarianten: document.querySelector('#KleurVarianten').value,
-            // GarantieInMaand: document.querySelector('#GarantieInMaand').value,
-        };
+    document.getElementById('bikeForm').addEventListener('submit', function (e) {
+        e.preventDefault();
 
         $.ajax({
             url: '{{ route('store-bike') }}',
             type: 'POST',
-            data: data,
-            success: function(response) {
-                console.log('Success:', response);
-                toastr.success('Fiets is aangemaakt!');
+            data: {
+                Naam: $('#Naam').val(),
+                Prijs: $('#Prijs').val(),
+                Voorraad: $('#Voorraad').val(),
+                Productcategorieën: $('#Productcategorieën').val(),
+                Merk: $('#Merk').val(),
+                SN: $('#SN').val(),
+                FrameMateriaal: $('#FrameMateriaal').val(),
+                BatterijTypen: $('#BatterijTypen').val(),
+                WielMaat: $('#WielMaat').val(),
+                Versnelling: $('#Versnelling').val(),
+                KleurVarianten: $('#KleurVarianten').val(),
+                GarantieInMaand: $('#GarantieInMaand').val(),
             },
-            error: function(error) {
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            success: function (response) {
+                console.log('Success:', response);
+                toastr.success(response.message);
+            },
+            error: function (error) {
                 console.error('Error:', error);
-                toastr.error('Er is iets fout gegaan bij het aanmaken van de fiets: ' + error.responseJSON.message);
+                toastr.error(error.responseJSON.message);
             }
         });
+
     });
 </script>
