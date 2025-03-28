601d6739d3d6b82b84da048719eea6fdcb53d310
MarcelJVermeer1 <150677934+MarcelJVermeer1@users.noreply.github.com>
2025-03-28 12:37:32 +0100

    login work but custome login doesnt

--- resources/views/customerAuth/register.blade.php
+++ resources/views/customerAuth/register.blade.php
@@ -4,6 +4,9 @@
     <input type="email" name="email" placeholder="Email" required><br>
     <input type="password" name="password" placeholder="Password" required><br>
     <input type="password" name="password_confirmation" placeholder="Bevestig het wachtwoord" required><br>
+    <input type="text" name="telefoon" placeholder="Telefoon"><br>
+    <input type="text" name="adres" placeholder="Adres"><br>
+    <input type="text" name="postcode" placeholder="Postcode">
     <button type="submit">Register</button>
     <br>
     <a href="{{ route('login') }}">Login</a>
